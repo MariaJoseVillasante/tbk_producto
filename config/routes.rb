13c142e5285ee5657352024a99adc234447bf271
MariaJoseVillasante <pepavillasante@yahoo.com>
2023-01-24 18:38:08 -0300

    agregando devise y producto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
+  devise_for :admins
+  get 'home/index'
+  root to: "home#index"
+  resources :productos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
